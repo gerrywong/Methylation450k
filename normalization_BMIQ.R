19470ebe2f2ed24a25060a449b658657ab0bdbc6
gerrywong <gerrywong1988@users.noreply.github.com>
2016-12-03 23:48:01 +0800

    lmFit beta_BMIQ

--- normalization_BMIQ.R
+++ normalization_BMIQ.R
@@ -3,6 +3,7 @@ filedir<-argv[1]
 files<-list.files(filedir)
 require(minfi)
 require(RPMM)
+require(data.table)
 source("BMIQ_1.3.R")
 for(i in 1:length(files)){
   file<-files[i]
@@ -19,3 +20,52 @@ for(i in 1:length(files)){
   towrite<-cbind(d,beta_BMIQ)
   write.table(towrite,file=paste0(sample,"_BMIQ.txt"),quote=F,sep='\t')
 }
+
+bt<-matrix(rep(NA,485577*836),nrow=485577)
+probe<-read.table("~/Data/BRCA/methy450k/tumor/jhu-usc.edu_BRCA.HumanMethylation450.9.lvl-3.TCGA-BH-A1F0-01A-11D-A138-05.txt",header=T,sep='\t')
+probe<-probe[,1]
+probe<-probe[-1]
+rownames(bt)<-probe
+require(data.table)
+require(limma)
+path_tumor<-"~/BRCAme450k/tumor"
+path_normal<-"~/BRCAme450k/normal"
+files_tumor<-list.files(path_tumor)
+files_normal<-list.files(path_normal)
+filespath_tumor<-paste(path_tumor,files_tumor,sep='/')
+filespath_normal<-paste(path_normal,files_normal,sep='/')
+filespath<-c(filespath_tumor,filespath_normal)
+for(i in 1:length(filespath)){
+  temp<-fread(filespath[i],sep='\t',skip=1)
+  bt[temp[,V1],i]<-temp[,V4]
+  rm(temp)
+  print(i)
+  gc()
+}
+
+num<-apply(bt,1,function(x) length(which(is.na(x)=="FALSE")))
+bt<-bt[num==836,]
+
+type<-c(rep("tumor",740),rep("normal",96))
+design<-model.matrix(~factor(type))
+colnames(design)<-c("Intercept","tumor")
+
+fit<-lmFit(bt,design)
+fit<-eBayes(fit)
+
+design1<-design[,2]
+fit1<-lmFit(bt,design1)
+fit1<-eBayes(fit1)
+
+
+
+
+
+
+
+
+
+
+
+
+
